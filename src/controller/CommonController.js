77ce59c4d81ff50802dc70385e81dd878aede9ea
otgoo1994 <62593887+otgoo1994@users.noreply.github.com>
2022-08-29 17:09:03 +0800

    add fixes

--- src/controller/CommonController.js
+++ src/controller/CommonController.js
@@ -55,17 +55,30 @@ const download = async (req, res) => {
         
     const file = await FileModel.loadFile(requestData, fileId);
    try {
-        const rs = fs.createReadStream(directoryPath + file[0].file_name);
-        res.setHeader("Content-Disposition", "attachment; " + file[0].original_file_name );
-        rs.pipe(res);
-        // await res.download(directoryPath + file[0].file_name , file[0].file_name , (err) => {
-        //     if (err) {
-        //         responseData.setResponseCode(RESPONSE_CODE.CONTACT_ADMIN);
-        //         res.send(responseData);
-        //     } else {
-        //         responseData.setResponseCode(RESPONSE_CODE.OK);
-        //     }
-        // });
+
+        if (file.length > 0) {
+            if (file[0].file_name) {
+                const rs = fs.createReadStream(directoryPath + file[0].file_name);
+                res.setHeader("Content-Disposition", "attachment;filename=\"" + file[0].original_file_name +"\"");
+                rs.pipe(res);
+                // await res.download(directoryPath + file[0].file_name , file[0].file_name , (err) => {
+                //     if (err) {
+                //         responseData.setResponseCode(RESPONSE_CODE.CONTACT_ADMIN);
+                //         res.send(responseData);
+                //     } else {
+                //         responseData.setResponseCode(RESPONSE_CODE.OK);
+                //     }
+                // });
+            } else {
+                await requestData.error();
+                responseData.setResponseCode(RESPONSE_CODE.NO_DATA);
+                res.send(responseData);
+            }
+        } else {
+            await requestData.error();
+            responseData.setResponseCode(RESPONSE_CODE.NO_DATA);
+            res.send(responseData);
+        }
      } catch (e) {
         Logger.debug(e);
         await requestData.error();
